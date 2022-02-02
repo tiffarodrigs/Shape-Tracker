32022f6f7a8fd718486f334ea0ad7e6f5e34ae69
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-02-01 22:35:56 -0800

    Update rectangle test case

--- _test_/rectangle.test.js
+++ _test_/rectangle.test.js
@@ -1,12 +1,17 @@
 
 import Rectangle from "../src/js/rectangle";
 describe('Rectangle', () =>{
+  let rectangle
+beforeEach(()=>{
+   rectangle=new Rectangle(5,3);
 
+});
  test('Should create a rectangle object with two lengths',() => {
-  const rectangle=new Rectangle(5,3);
   expect(rectangle.side1).toEqual(5);
   expect(rectangle.side2).toEqual(3);
  });
-
+ test('should correctly get the area of a rectangle object', () => {
+  expect(rectangle.getArea()).toEqual(15);
+});
 
 });
